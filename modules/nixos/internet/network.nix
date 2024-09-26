e835969c7bda71e5398830480ee0c300bec5f37f
oliverst8 <oliver@starup99.dk>
2024-09-26 20:17:34 +0200

    Added comments for different wpa versions

--- modules/nixos/internet/network.nix
+++ modules/nixos/internet/network.nix
@@ -10,7 +10,8 @@
         name = "Network_manager_with_2_9_wpa_supplicant";
         url = "https://github.com/NixOS/nixpkgs/";
         ref = "refs/heads/nixos-24.05";
-        rev = "0229135d7a2eedc5490cafb8c61562f71c6d357b";
+        rev = "0229135d7a2eedc5490cafb8c61562f71c6d357b"; #Wpa version 2.9
+        #rev = "270dace49bc95a7f88ad187969179ff0d2ba20ed"; #wpa version 2.10
       }) {
         system = "x86_64-linux";  # Specify your system architecture here
         }).wpa_supplicant;
