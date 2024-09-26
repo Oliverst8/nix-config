96f51fca7c445638b63c53a495d4e6cb3a3a2b51
oliverst8 <oliver@starup99.dk>
2024-09-26 20:18:59 +0200

    Add libinput package and dependencies for gestures
    This was done to allow mousepad gestures on hyprland

---
+++ modules/nixos/controlhub/libinput.nix
@@ -0,0 +1,14 @@
+{pkgs, ...}:
+{
+  services = {
+    xserver.windowManager.fvwm2.gestures = true;
+    libinput.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    libinput
+    libinput-gestures
+    wmctrl
+    xdotool
+  ];
+}
