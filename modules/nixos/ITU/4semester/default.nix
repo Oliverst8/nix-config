e4c50106d334d087bc8c5e1603f670acb9acfaca
oliverst8 <oliver@starup99.dk>
2025-01-29 16:43:25 +0100

    Allowed unsafe package dotnet 7 and installed java

--- modules/nixos/ITU/4semester/default.nix
+++ modules/nixos/ITU/4semester/default.nix
@@ -4,5 +4,10 @@
 environment.systemPackages = [
     pkgs.jetbrains.rider
     pkgs.dotnet-sdk_7
+    pkgs.zulu21
   ];
+
+nixpkgs.config.permittedInsecurePackages = [
+                "dotnet-sdk-7.0.410"
+              ];
 }
