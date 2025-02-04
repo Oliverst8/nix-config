555ad0bd9cd89d8e4fe4ca855fc3d2c24cdb29ab
oliverst8 <oliver@starup99.dk>
2025-02-04 20:14:01 +0100

    Updated zsh config

--- modules/home-manager/terminal/zsh.nix
+++ modules/home-manager/terminal/zsh.nix
@@ -1,21 +1,40 @@
-{config, ...}:
+{ lib, config, ... }:
 
+let
+  cfg = config.zsh;
+in
 {
-programs.zsh = {
-  enable = true;
-  enableCompletion = true;
-  autosuggestion.enable = true;
-  syntaxHighlighting.enable = true;
-
-  shellAliases = {
-    ll = "ls -l";
-    update = "sudo nixos-rebuild switch --flake ~/nixos/#laptop";
+  options = {
+    zsh.enable = lib.mkEnableOption "Enable zsh";
+    zsh.dualboot = lib.mkEnableOption "Enables grub aliases";
   };
-  history = {
-    size = 10000;
-    path = "${config.xdg.dataHome}/zsh/history";
+  config = lib.mkIf cfg.enable {
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      shellAliases = {
+        ll = "ls -l";
+        update="sudo nixos-rebuild switch --flake ~/nixos/#laptop";
+      };
+
+      shellAliases.windows = lib.mkIf cfg.dualboot "sudo grub-reboot 1 && reboot";
+
+      sessionVariables = {
+        PATH = "$PATH:/home/david/.dotnet/tools";
+        DOTNET_CLI_TELEMETRY_OPTOUT = 1;
+      };
+      #history = {
+      #  size = 10000;
+      #  path = "${config.xdg.dataHome}/zsh/history";
+      #};
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ ];
+      };
+    };
   };
-
-};
-
 }
+
