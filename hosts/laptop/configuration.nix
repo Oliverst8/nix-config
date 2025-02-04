5a42d0961b5546e4a7119603ec866348ae06f62d
oliverst8 <oliver@starup99.dk>
2025-02-04 20:33:29 +0100

    Enabled zsh

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -129,7 +129,7 @@
     services.libinput.enable = true;
 
     # Enbale zsh
-    #programs.zsh.enable = true;
+    programs.zsh.enable = true;
 
     # Define a user account. Don't forget to set a password with ‘passwd’.
     users.users.ostarup = {
