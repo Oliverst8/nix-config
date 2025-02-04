9b086e315b0b53796ee95050a6bbe0ada3d8bb83
oliverst8 <oliver@starup99.dk>
2025-02-04 20:33:36 +0100

    Updated zsh.nix

--- modules/home-manager/terminal/zsh.nix
+++ modules/home-manager/terminal/zsh.nix
@@ -6,33 +6,38 @@ in
 {
   options = {
     zsh.enable = lib.mkEnableOption "Enable zsh";
-    zsh.dualboot = lib.mkEnableOption "Enables grub aliases";
   };
   config = lib.mkIf cfg.enable {
     programs.zsh = {
       enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
+      #enableCompletion = true;
+      #autosuggestion.enable = true;
+      #syntaxHighlighting.enable = true;
 
       shellAliases = {
         ll = "ls -l";
         update="sudo nixos-rebuild switch --flake ~/nixos/#laptop";
       };
 
-      shellAliases.windows = lib.mkIf cfg.dualboot "sudo grub-reboot 1 && reboot";
 
-      sessionVariables = {
-        PATH = "$PATH:/home/david/.dotnet/tools";
-        DOTNET_CLI_TELEMETRY_OPTOUT = 1;
-      };
       #history = {
       #  size = 10000;
       #  path = "${config.xdg.dataHome}/zsh/history";
       #};
       oh-my-zsh = {
         enable = true;
-        plugins = [ ];
+        plugins = [ 
+          "git"
+          "sudo"
+          "copypath"
+          "copyfile"
+          "colorize"
+          "colored-man-pages"
+          "gh"
+          "zsh-autosuggestions"
+          "fast-syntax-highlighting"
+          "zsh-autocomplete"
+        ];
       };
     };
   };
