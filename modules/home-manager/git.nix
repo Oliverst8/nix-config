3fe7ffe01b8ff38c70c781c5982c6acb9ae1bf47
Oliver Starup <Oliver@starup99.dk>
2025-02-06 20:28:32 +0100

    Updated git username and email

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -2,19 +2,12 @@
 {
   programs.git = {
     enable = true;
-    userName = "oliverst8";
-    userEmail = "oliver@starup99.dk";
+    userName = "Oliver Starup";
+    userEmail = "Oliver@starup99.dk";
     
     extraConfig = {
       init.defaultBranch = "main";
       credential.helper = "!gh auth git-credential";
     };
-    
-    includes = [
-      {
-        condition = "gitdir:~/ITU/";
-        path = "~/ITU/.gitconfig";
-      }
-    ];
   };
 }
