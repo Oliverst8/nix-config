5de665b783feb5c146a05a20b880cfd21afb80e3
oliverst8 <oliver@starup99.dk>
2025-02-06 20:05:02 +0100

    Added both option for kde and hyprland DE

--- modules/bundles/desktop-environment/desktop-environment.nix
+++ modules/bundles/desktop-environment/desktop-environment.nix
@@ -8,7 +8,7 @@
      
   config = {
     #assert assertMsg ((config.desktop.environment or "") == "") "Error: config.desktop.environment must be set to a non-empty value."; 
-    kde.enable = (if (config.desktop.environment == "kde") then true else false);
-    hyprland-module.enable = (if (config.desktop.environment == "hyprland") then true else false);
+    kde.enable = (if (config.desktop.environment == "kde" || config.desktop.environment == "both") then true else false);
+    hyprland-module.enable = (if (config.desktop.environment == "hyprland" || config.desktop.environment == "both") then true else false);
   };
 }
