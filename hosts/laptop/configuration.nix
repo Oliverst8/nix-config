823422309e86d42695c12028e0e942236bd59562
Oliver Starup <Oliver@starup99.dk>
2025-02-22 09:43:11 +0100

    Changed import to just be folder

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -11,7 +11,7 @@
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
 
-    ../../modules/nixos/common/default.nix
+    ../../modules/nixos/common
   ];
 
   config = {
