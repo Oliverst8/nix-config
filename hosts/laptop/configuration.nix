8d8f60d35afc0c63d5940b16af240da012233f52
Oliver Starup <Oliver@starup99.dk>
2025-02-22 07:48:31 +0100

    Changed path to common default

--- hosts/laptop/configuration.nix
+++ hosts/laptop/configuration.nix
@@ -11,7 +11,7 @@
   imports = [ # Include the results of the hardware scan.
     ./hardware-configuration.nix
 
-    ../../modules/common/default.nix
+    ../../modules/nixos/common/default.nix 
   ];
 
   config = {
