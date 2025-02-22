ed64b1ee860f8163442353965721bfe56d5479fc
Oliver Starup <Oliver@starup99.dk>
2025-02-22 17:48:47 +0100

    Updated commands to real path

--- modules/nixos/common/genPackageFile.nix
+++ modules/nixos/common/genPackageFile.nix
@@ -13,11 +13,11 @@
       echo "" >> "$output_file"
 
       # Get direct dependencies
-      direct_deps=$(nix-store -q --references /var/run/current-system/sw | cut -d'-' -f2-)
+      direct_deps=$(/run/current-system/sw/bin/nix-store -q --references /var/run/current-system/sw | cut -d'-' -f2-)
 
       # Remove matches of the regex pattern and sort uniquely
-      filtered_deps=$(echo "$direct_deps" | sed 's/-\(bin\|doc\|man\|info\)//g' | sort -u)
-      filtered_deps_with_space=$(echo "$filtered_deps" | sed 's/-\([0-9]\)/ \1/')
+      filtered_deps=$(echo "$direct_deps" | /run/current-system/sw/bin/sed 's/-\(bin\|doc\|man\|info\)//g' | sort -u)
+      filtered_deps_with_space=$(echo "$filtered_deps" | /run/current-system/sw/bin/sed 's/-\([0-9]\)/ \1/')
 
       # Write Direct dependencies to the output file
       echo "## Direct Dependencies" >> "$output_file"
