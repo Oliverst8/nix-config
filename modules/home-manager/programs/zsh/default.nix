3d5beffd6fcfd803bfa0d7bc7b0e6e7a2572551c
Oliver Starup <Oliver@starup99.dk>
2025-02-22 09:44:07 +0100

    Updated zsh.nix

--- modules/home-manager/programs/zsh/default.nix
+++ modules/home-manager/programs/zsh/default.nix
@@ -1,6 +1,9 @@
-{ ... }: {
+{ lib, config, ... }:
+
+let cfg = config.zsh;
+in {
   options = { zsh.enable = lib.mkEnableOption "Enable zsh"; };
-  config = lib.mkIf config.zsh.enable {
+  config = lib.mkIf cfg.enable {
     programs.zsh = {
       enable = true;
       enableCompletion = true;
@@ -15,6 +18,11 @@
         s = "spotify_player";
         sus = "python -c 'print(chr(sum(range(ord(min(str(not())))))))'";
       };
+
+      #history = {
+      #  size = 10000;
+      #  path = "${config.xdg.dataHome}/zsh/history";
+      #};
       oh-my-zsh = {
         enable = true;
         plugins = [
@@ -30,3 +38,4 @@
     };
   };
 }
+
