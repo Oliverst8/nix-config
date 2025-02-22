4a777e9eaaeb60e08529d01eac4cac1e3d71fe1e
Oliver Starup <Oliver@starup99.dk>
2025-02-22 09:42:53 +0100

    Deleted files

--- modules-backup/home-manager/terminal/alacritty.nix
+++
@@ -1,6 +0,0 @@
-{ inputs, pkg, config, ... }:
-{
-	home.file.".config/alacritty/" = {
-	 	source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos/dotfiles/.config/alacritty";
-	};
-}
