45800b567cd9a7e94f03071552c90c6284429241
Oliver Starup <Oliver@starup99.dk>
2025-02-26 20:19:31 +0100

    Removed test option

--- options/default.nix
+++ options/default.nix
@@ -3,11 +3,6 @@
 with lib;
 
 {
-  options.test.enable = mkOption {
-    type = types.str;
-    description = "Test option";
-    default = false;
-  };
   options.gottis.enable = mkOption {
     type = types.bool;
     description = "Enable gottis";
