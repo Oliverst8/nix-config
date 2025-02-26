3b20b92feaff8d96186ba2accbf827c9c40a4510
Oliver Starup <Oliver@starup99.dk>
2025-02-26 11:34:28 +0100

    Added htop

--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -151,13 +151,14 @@
       pkgs.mpv # Video player
       pkgs.direnv
       pkgs.nixfmt-rfc-style # Formatter for nix
-      pkgs.nil #Nix language server
+      pkgs.nil # Nix language server
       pkgs.tree # Tree command
       pkgs.docker
       pkgs.ghostty # Terminal
       pkgs.starship # Terminal theme
       pkgs.tlp # Makes laptop more power efficent
       pkgs.unzip
+      pkgs.htop
 
       pkgs.yazi # Tui file manager
       pkgs.spotify-player # TUI spotfiy https://github.com/aome510/spotify-player
