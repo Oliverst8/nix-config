6801726a0aa19d69647d4b24303a902b0a61c800
Oliver Starup <Oliver@starup99.dk>
2025-02-27 16:49:39 +0100

    Added git options

--- options/default.nix
+++ options/default.nix
@@ -8,4 +8,19 @@ with lib;
     description = "Enable gottis";
     default = false;
   };
+  options.git.enable = mkOption {
+    type = types.bool;
+    description = "Enable git";
+    default = false;
+  };
+  options.git.lazygit.enable = mkOption {
+    type = types.bool;
+    description = "Enable lazygit (Only works if git is enabled)";
+    default = true;
+  };
+  options.git.gh.enable = mkOption {
+    type = types.bool;
+    description = "Enable the github cli (Only works if git is enabled)";
+    default = true;
+  };
 }
