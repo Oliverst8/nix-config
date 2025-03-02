42a66ac8d21c2423d79f13253caa19de5153ca3f
Oliver Starup <Oliver@starup99.dk>
2025-03-02 19:30:53 +0100

    Formated flake.nix

--- flake.nix
+++ flake.nix
@@ -3,21 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-nixos-wsl = {
+    nixos-wsl = {
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
-    };    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
+    };
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     hyprland-plugins = {
-        url = "github:hyprwm/hyprland-plugins";
-        inputs.hyprland.follows = "hyprland";
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
 
     };
 
-     home-manager = {
-       url = "github:nix-community/home-manager";
-       inputs.nixpkgs.follows = "nixpkgs";
-     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
     sddm-sugar-candy-nix = {
       url = "gitlab:Zhaith-Izaliel/sddm-sugar-candy-nix";
@@ -26,36 +27,43 @@ nixos-wsl = {
 
   };
 
-  outputs = { self, nixpkgs, nixos-wsl, ... }@inputs: {
-    nixosConfigurations = {
-      default = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/default/configuration.nix
-        #./hosts/laptop/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
-     };
-      laptop = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/laptop/configuration.nix
-        #./hosts/laptop/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
-     };
-     wsl = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-	specialArgs = { inherit inputs; }; 
-        modules = [
-	./hosts/wsl/configuration.nix
-        inputs.home-manager.nixosModules.default
-          nixos-wsl.nixosModules.default
-          {
-            wsl.enable = true;
-          }
-        ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixos-wsl,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations = {
+        default = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/default/configuration.nix
+            #./hosts/laptop/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+        laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/laptop/configuration.nix
+            #./hosts/laptop/configuration.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+        wsl = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/wsl/configuration.nix
+            inputs.home-manager.nixosModules.default
+            nixos-wsl.nixosModules.default
+            {
+              wsl.enable = true;
+            }
+          ];
+        };
       };
     };
-  };
 }
