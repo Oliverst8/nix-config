29b0c8d4e9ad8ec93e31bf76ef33a827df281da8
Oliver Starup <Oliver@starup99.dk>
2025-03-19 17:22:16 +0100

    Added copilot chat file

---
+++ modules/home-manager/programs/neovim/lua/custom/plugins/copilotChat.lua
@@ -0,0 +1,32 @@
+return {
+  "CopilotC-Nvim/CopilotChat.nvim",
+  dependencies = {
+    { "nvim-lua/plenary.nvim" },
+    { "github/copilot.vim" },
+  },
+  opts = {
+    -- Add any configuration options here
+    window = {
+      -- How to open the chat window
+      layout = "float", -- 'float' | 'split' | 'vsplit'
+      -- Other window options
+      border = "single", -- 'none' | 'single' | 'double' | 'rounded' | 'solid' | 'shadow'
+      title = " Copilot Chat ",
+      width = 0.8, -- fractional width of parent window (number between 0 and 1)
+      height = 0.6, -- fractional height of parent window (number between 0 and 1)
+    },
+    -- ... other configuration options
+  },
+  -- The plugin is already installed via Nix, so we don't need to add a build step
+  cmd = {
+    "CopilotChat",
+    "CopilotChatToggle",
+    "CopilotChatOpen",
+    "CopilotChatClose",
+  },
+  keys = {
+    -- Add keymaps for quick access
+    { "<leader>cc", "<cmd>CopilotChat<CR>", desc = "CopilotChat - Open" },
+    { "<leader>ct", "<cmd>CopilotChatToggle<CR>", desc = "CopilotChat - Toggle" },
+  },
+}
