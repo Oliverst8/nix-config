b62fb5597cd5617077cab5d7401f2af50868386c
Oliver Starup <Oliver@starup99.dk>
2025-03-21 23:09:31 +0100

    Made test window flaot

--- modules/home-manager/programs/neovim/lua/custom/plugins/easy-dotnet.lua
+++ modules/home-manager/programs/neovim/lua/custom/plugins/easy-dotnet.lua
@@ -3,6 +3,12 @@ return -- lazy.nvim
   'GustavEikaas/easy-dotnet.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
   config = function()
-    require('easy-dotnet').setup()
+    require('easy-dotnet').setup {
+      ---@type TestRunnerOptions
+      test_runner = {
+        ---@type "split" | "float" | "buf"
+        viewmode = 'float',
+      },
+    }
   end,
 }
