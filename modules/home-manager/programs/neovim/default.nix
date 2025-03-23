a1fa0c3bdfd6f1bd1bebeae503f7b5509f1c0757
Oliver Starup <Oliver@starup99.dk>
2025-03-23 17:46:29 +0100

    Added java lsp for neovim

--- modules/home-manager/programs/neovim/default.nix
+++ modules/home-manager/programs/neovim/default.nix
@@ -69,6 +69,7 @@ in
               roslyn-ls
               fsautocomplete
               fantomas
+              jdt-language-server
             ];
             kickstart-debug = [
               delve
