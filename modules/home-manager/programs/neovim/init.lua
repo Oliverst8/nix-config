a7f8ff98dc95289f5869854685ea99035d43f709
Oliver Starup <Oliver@starup99.dk>
2025-03-26 10:27:23 +0100

    Changed to non depricated tsserver

--- modules/home-manager/programs/neovim/init.lua
+++ modules/home-manager/programs/neovim/init.lua
@@ -649,7 +649,7 @@ require('nixCatsUtils.lazyCat').setup(nixCats.pawsible { 'allPlugins', 'start', 
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       -- NOTE: nixCats: there is help in nixCats for lsps at `:h nixCats.LSPs` and also `:h nixCats.luaUtils`
-      local servers = { pyright = {}, fsautocomplete = {}, jdtls = {}, eslint = {}, tsserver = {} }
+      local servers = { pyright = {}, fsautocomplete = {}, jdtls = {}, eslint = {}, ts_ls = {} }
       -- servers.clangd = {},
       -- servers.gopls = {},
       -- servers.pyright = {},
