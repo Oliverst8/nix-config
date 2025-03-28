531facecb856164b0524f7adc159d39ddcf7bdf0
Oliver Starup <Oliver@starup99.dk>
2025-03-28 21:41:49 +0100

    Moved more plugins to own files

--- modules/home-manager/programs/neovim/lua/kickstart/plugins/gitsigns.lua
+++ modules/home-manager/programs/neovim/lua/kickstart/plugins/gitsigns.lua
@@ -6,8 +6,16 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     -- NOTE: nixCats: return true only if category is enabled, else false
-    enabled = require('nixCatsUtils').enableForCategory("kickstart-gitsigns"),
+    enabled = require('nixCatsUtils').enableForCategory 'kickstart-gitsigns',
     opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+
       on_attach = function(bufnr)
         local gitsigns = require 'gitsigns'
 
