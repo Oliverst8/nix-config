704b29cf8a45528d957c2010c04118d56305aa0c
Oliver Starup <Oliver@starup99.dk>
2025-04-17 13:15:18 +0200

    Updated firefox config

--- modules/home-manager/programs/firefox/default.nix
+++ modules/home-manager/programs/firefox/default.nix
@@ -16,14 +16,41 @@
         name = "default";
         isDefault = true;
         settings = {
-          #"browser.startup.homepage" = "https://google.com";
           "browser.search.defaultenginename" = "Google";
           "browser.search.order.1" = "Google";
-          "extensions.autoDisableScopes" = 0;
+          "extensions.autoDisableScopes" = 0; # No Extension warning first open
+          "signon.rememberSignons" = false; # Don't ask to remember logins
+
+          #Darkmode
           "layout.css.prefers-color-scheme.content-override" = 0;
           "browser.theme.toolbar-theme" = 0;
           "browser.theme.content-theme" = 0;
           "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+
+          "privacy.trackingprotection.enabled" = true; # Strict / custom tracking protection
+          # Mozilla Telemetry disable https://github.com/K3V1991/Disable-Firefox-Telemetry-and-Data-Collection
+          "browser.newtabpage.activity-stream.feeds.telemetry" = false;
+          "browser.newtabpage.activity-stream.telemetry" = false;
+          "browser.ping-centre.telemetry" = false;
+          "datareporting.healthreport.service.enabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "datareporting.policy.dataSubmissionEnabled" = false;
+          "datareporting.sessions.current.clean" = true;
+          "devtools.onboarding.telemetry.logged" = false;
+          "toolkit.telemetry.archive.enabled" = false;
+          "toolkit.telemetry.bhrPing.enabled" = false;
+          "toolkit.telemetry.enabled" = false;
+          "toolkit.telemetry.firstShutdownPing.enabled" = false;
+          "toolkit.telemetry.hybridContent.enabled" = false;
+          "toolkit.telemetry.newProfilePing.enabled" = false;
+          "toolkit.telemetry.prompted" = 2;
+          "toolkit.telemetry.rejected" = true;
+          "toolkit.telemetry.reportingpolicy.firstRun" = false;
+          "toolkit.telemetry.server" = "";
+          "toolkit.telemetry.shutdownPingSender.enabled" = false;
+          "toolkit.telemetry.unified" = false;
+          "toolkit.telemetry.unifiedIsOptIn" = false;
+          "toolkit.telemetry.updatePing.enabled" = false;
         };
         extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
           ublock-origin
