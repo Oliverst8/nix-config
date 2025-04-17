98a42b3907517ed10b15b545e7cfb37aaf7ba689
Oliver Starup <Oliver@starup99.dk>
2025-04-17 11:27:56 +0200

    Made darkmode be on in firefox

--- modules/home-manager/programs/firefox/default.nix
+++ modules/home-manager/programs/firefox/default.nix
@@ -16,12 +16,14 @@
         name = "default";
         isDefault = true;
         settings = {
-          "browser.startup.homepage" = "https://google.com";
+          #"browser.startup.homepage" = "https://google.com";
           "browser.search.defaultenginename" = "Google";
           "browser.search.order.1" = "Google";
           "extensions.autoDisableScopes" = 0;
-          #"layout.css.prefers-color-scheme.content-override" = 0;
-
+          "layout.css.prefers-color-scheme.content-override" = 0;
+          "browser.theme.toolbar-theme" = 0;
+          "browser.theme.content-theme" = 0;
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
         };
         extensions.packages = with inputs.firefox-addons.packages.${pkgs.system}; [
           ublock-origin
