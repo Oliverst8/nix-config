49a49006282f747240a91c56a9baf3146031b9d5
Oliver Starup <Oliver@starup99.dk>
2025-04-28 20:56:51 +0200

    Stopped waiting for network manager on boot

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -1,20 +1,27 @@
-{pkgs, ...}:
+{ pkgs, ... }:
 {
   # Enable networking
   networking.networkmanager.enable = true;
 
-    # Overlay to pin NetworkManager to the nixos-24.05 channel
+  #Disable waiting for network on boot
+  systemd.services.NetworkManager-wait-online.enable = false;
+
+  # Overlay to pin NetworkManager to the nixos-24.05 channel
   nixpkgs.overlays = [
     (self: super: {
-     wpa_supplicant = (import (builtins.fetchGit {
-        name = "Network_manager_with_2_9_wpa_supplicant";
-        url = "https://github.com/NixOS/nixpkgs/";
-        ref = "refs/heads/nixos-24.05";
-        rev = "0229135d7a2eedc5490cafb8c61562f71c6d357b"; #Wpa version 2.9
-        #rev = "270dace49bc95a7f88ad187969179ff0d2ba20ed"; #wpa version 2.10
-      }) {
-        system = "x86_64-linux";  # Specify your system architecture here
-        }).wpa_supplicant;
+      wpa_supplicant =
+        (import
+          (builtins.fetchGit {
+            name = "Network_manager_with_2_9_wpa_supplicant";
+            url = "https://github.com/NixOS/nixpkgs/";
+            ref = "refs/heads/nixos-24.05";
+            rev = "0229135d7a2eedc5490cafb8c61562f71c6d357b"; # Wpa version 2.9
+            #rev = "270dace49bc95a7f88ad187969179ff0d2ba20ed"; #wpa version 2.10
+          })
+          {
+            system = "x86_64-linux"; # Specify your system architecture here
+          }
+        ).wpa_supplicant;
 
     })
   ];
