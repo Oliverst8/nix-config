fb9e2498d9f6f599dd144475ac2d556f57ff82b9
Oliver Starup <Oliver@starup99.dk>
2025-09-06 20:36:16 +0200

    Added oh my tmux hash

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -5,7 +5,7 @@ let
     owner = "gpakosz";
     repo = ".tmux";
     rev = "master"; # or "master" if you want latest
-    sha256 = "sha256-hash-here"; # run `nix-prefetch-git` or `nix-prefetch-url` to get this
+    sha256 = "0nzcfi7413mzh0ka59199h71ky6nrvgfy3aj8dhddbpb5cyv4y2x"; # run `nix-prefetch-git` or `nix-prefetch-url` to get this
   };
 in
 {
