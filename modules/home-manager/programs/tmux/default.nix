c53bbcdda782ac2df6e2662e9900ad58210a21ad
Oliver Starup <Oliver@starup99.dk>
2025-09-14 17:57:23 +0200

    Working

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -5,37 +5,35 @@
     enable = true;
     keyMode = "vi";
     baseIndex = 1;
-    clock24 = true;
     plugins = with pkgs; [
       {
         plugin = tmuxPlugins.tokyo-night-tmux;
         extraConfig = "
-	  set -g @plugin 'fabioluciano/tmux-tokyo-night'
-	### Tokyo Night Theme configuration
-set -g @theme_variation 'moon'
-set -g @theme_left_separator ''
-set -g @theme_right_separator ''
-set -g @theme_plugins 'datetime,weather,playerctl,yay'
-	  ";
+          ## Tokyo Night Theme configuration
+          set -g @theme_variation 'moon'
+          set -g @theme_left_separator ''
+          set -g @theme_right_separator ''
+          set -g @theme_plugins 'datetime,weather,playerctl,yay'
+          	  ";
       }
     ];
-    extraConfig = "
-			# Bind `prefix + v` to enter copy-mode
-			bind v copy-mode
-
-			# In copy-mode (vi keys), pressing i cancels/exits copy-mode
-			bind -T copy-mode-vi 'i' send-keys -X cancel
-
-			# In copy-mode, `v` starts a selection (like vim visual mode)
-			bind-key -T copy-mode-vi 'v' send -X begin-selection
-
-			# In copy-mode, Esacpe cancels selections
-			bind-key -T copy-mode-vi Escape send -X clear-selection
-
-			# In copy-mode, `y` copies the selection and clears it,
-			# but does NOT exit copy-mode — only i can exit
-			bind-key -T copy-mode-vi 'y' send -X copy-selection-no-clear \; send -X clear-selection
-
-			";
+    #   extraConfig = "
+    # 	# Bind `prefix + v` to enter copy-mode
+    # 	bind v copy-mode
+    #
+    # 	# In copy-mode (vi keys), pressing i cancels/exits copy-mode
+    # 	bind -T copy-mode-vi 'i' send-keys -X cancel
+    #
+    # 	# In copy-mode, `v` starts a selection (like vim visual mode)
+    # 	bind-key -T copy-mode-vi 'v' send -X begin-selection
+    #
+    # 	# In copy-mode, Esacpe cancels selections
+    # 	bind-key -T copy-mode-vi Escape send -X clear-selection
+    #
+    # 	# In copy-mode, `y` copies the selection and clears it,
+    # 	# but does NOT exit copy-mode — only i can exit
+    # 	bind-key -T copy-mode-vi 'y' send -X copy-selection-no-clear \; send -X clear-selection
+    #
+    # 	";
   };
 }
