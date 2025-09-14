8adfec794b7cb2bcfa9e91b7a0846534dfb57c0d
Oliver Starup <Oliver@starup99.dk>
2025-09-14 16:46:15 +0200

    Added options to mux

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -3,5 +3,31 @@
 {
   programs.tmux = {
     enable = true;
+    keyMode = "vi";
+    baseIndex = 1;
+    clock24 = true;
+    prefix = "C-a";
+    extraConfig = "
+			# Bind `prefix + v` to enter copy-mode
+			bind v copy-mode
+
+			# In copy-mode (vi keys), pressing i cancels/exits copy-mode
+			bind -T copy-mode-vi 'i' send-keys -X cancel
+
+			# In copy-mode, `v` starts a selection (like vim visual mode)
+			bind-key -T copy-mode-vi 'v' send -X begin-selection
+
+			# In copy-mode, Esacpe cancels selections
+			bind-key -T copy-mode-vi Escape send -X clear-selection
+
+			# In copy-mode, `y` copies the selection and clears it,
+			# but does NOT exit copy-mode — only i can exit
+			bind-key -T copy-mode-vi 'y' send -X copy-selection-no-clear \; send -X clear-selection
+
+			# Bind Escape to enter copy-mode without prefix
+			bind-key -n Escape copy-mode
+
+
+			";
   };
 }
