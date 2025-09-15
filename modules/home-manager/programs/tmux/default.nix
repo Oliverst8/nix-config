9b7f969630669ffe1f3976f3c72749365240644d
Oliver Starup <Oliver@starup99.dk>
2025-09-15 12:58:44 +0200

    Added own file for tmux tokyo night theme

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -1,17 +1,7 @@
 { pkgs, ... }:
 
 let
-  tmux-tokyo-night = pkgs.tmuxPlugins.mkTmuxPlugin {
-    pluginName = "tmux-tokyo-night.tmux";
-    rtpFilePath = "tmux-tokyo-night.tmux";
-    version = "v1.11.0"; # or the latest release
-    src = pkgs.fetchFromGitHub {
-      owner = "fabioluciano";
-      repo = "tmux-tokyo-night";
-      rev = "v1.11.0"; # replace with the tag or commit you want
-      sha256 = "sha256-WjDbunWmxbw/jjvc34ujOWif18POC3WVO1s+hk9SLg4=";
-    };
-  };
+  tmux-tokyo-night = import ./tmux-tokyo-night.nix { inherit pkgs; };
 in
 {
   programs.tmux = {
