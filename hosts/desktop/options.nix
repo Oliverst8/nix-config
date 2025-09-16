592760827529037ed0a2420b8b14ff5554b7d496
Oliver Starup <Oliver@starup99.dk>
2025-09-16 14:54:16 +0200

    Added monitors to desktop

--- hosts/desktop/options.nix
+++ hosts/desktop/options.nix
@@ -11,6 +11,8 @@
     env.background = "~/nixos/imgs/.background/pure-black-background.jpg";
 
     desktop.hyprland.monitors = [
+      "desc:ASUSTek COMPUTER INC XG27AQDMG S6LMRS002976,2560x1440@239.97,0x0,1"
+      "DP-4, 2560x1440@144.00, auto-right, 1"
     ];
 
   };
