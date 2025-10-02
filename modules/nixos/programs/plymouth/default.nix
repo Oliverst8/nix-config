a395d96e16f22428035396481db60f8a2618561e
Oliver Starup <Oliver@starup99.dk>
2025-10-02 14:06:58 +0200

    Updated boot loader timeout

--- modules/nixos/programs/plymouth/default.nix
+++ modules/nixos/programs/plymouth/default.nix
@@ -23,7 +23,7 @@
     # Hide the OS choice for bootloaders.
     # It's still possible to open the bootloader list by pressing any key
     # It will just not appear on screen unless a key is pressed
-    loader.timeout = 0;
-
+    loader.timeout = 1;
+    loader.grub.timeoutStyle = "hidden"; # don’t show menu unless key pressed
   };
 }
