3e4ca116a28bf4c84f8380944beaa7bf966ffc88
Oliver Starup <Oliver@starup99.dk>
2025-10-21 11:23:45 +0200

    Updated git config syntax

--- modules/home-manager/programs/git/default.nix
+++ modules/home-manager/programs/git/default.nix
@@ -3,13 +3,15 @@
 {
   programs.git = lib.mkIf config.git.enable {
     enable = true;
-    userName = "Oliver Starup";
-    userEmail = "Oliver@starup99.dk";
-    aliases = {
-      undo = "reset --soft HEAD~1";
-    };
+    settings = {
+      user = {
+        name = "Oliver Starup";
+        email = "Oliver@starup99.dk";
+      };
+      alias = {
+        undo = "reset --soft HEAD~1";
+      };
 
-    extraConfig = {
       init.defaultBranch = "main";
       credential.helper = "!gh auth git-credential";
     };
