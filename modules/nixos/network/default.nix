d0723f8f6ba7d717dbfc67a733804657cd6b7556
Oliver Starup <Oliver@starup99.dk>
2025-11-01 08:44:16 +0100

    Preving wifi power save on sleep

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -6,6 +6,9 @@
   # Enable networking
   networking.networkmanager.enable = true;
 
+  # Fix no wifi after sleep
+  networking.networkmanager.wifi.powersave = false;
+
   #Disable waiting for network on boot
   systemd.services.NetworkManager-wait-online.enable = false;
 
