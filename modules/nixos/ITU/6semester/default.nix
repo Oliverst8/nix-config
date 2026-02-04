f5b70d84889c2985f23fa265be6894bb56a91867
Oliver Starup <Oliver@starup99.dk>
2026-02-04 11:46:54 +0100

    Removed cuda

--- modules/nixos/ITU/6semester/default.nix
+++ modules/nixos/ITU/6semester/default.nix
@@ -7,8 +7,6 @@
     pkgs.jetbrains.idea
     pkgs.android-studio-full
     pkgs.gradle
-    pkgs.cudaPackages.cuda_nvcc
-    pkgs.cudaPackages.cudatoolkit
   ];
 
   nixpkgs.config.android_sdk.accept_license = true;
