7a126f9fcd1bb9d83a14cfe727dd7ce8b43675bf
Oliver Starup <Oliver@starup99.dk>
2026-03-26 14:06:45 +0100

    Updated typescritp tools

--- modules/home-manager/programs/neovim/lua/custom/plugins/typescript-tools.lua
+++ modules/home-manager/programs/neovim/lua/custom/plugins/typescript-tools.lua
@@ -1,5 +1,25 @@
 return {
   'pmizio/typescript-tools.nvim',
   dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
-  opts = {},
+  opts = {
+    settings = {
+      -- Show inlay hints for parameter names, types, and return types
+      tsserver_file_preferences = {
+        includeInlayParameterNameHints = 'all',
+        includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+        includeInlayFunctionParameterTypeHints = true,
+        includeInlayVariableTypeHints = true,
+        includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+        includeInlayPropertyDeclarationTypeHints = true,
+        includeInlayFunctionLikeReturnTypeHints = true,
+        includeInlayEnumMemberValueHints = true,
+        -- Include completions for module exports
+        includeCompletionsForModuleExports = true,
+        -- Import styles
+        importModuleSpecifierPreference = 'non-relative',
+      },
+      -- Expose all TypeScript code actions as LSP code actions
+      expose_as_code_action = 'all',
+    },
+  },
 }
